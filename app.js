7b5421cf7d6231e328dc9f0e8b75f8b142ce6441
Vinay Summan <vinay.summan@rdpolytech.ca>
2025-08-13 18:17:14 -0600

    Update

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-
-const express = require('express');
+const express = require('express') ;
+const app = express();
 require('dotenv').config();
-const mongoose = require('require');
+const mongoose = require('mongoose');
+
 const PORT = process.env.PORT;
 const uri = process.env.MONGO_URI;
 
-app.listen(PORT, () => {
-  console.log(`Connected to port ${PORT}`);
-});
+mongoose.connect(uri).then(
+    async () => {
+
+        console.log('Connected to MongoDB');
+
+        app.listen(PORT, () => {
+        console.log(`Connected to port ${PORT}`);
+        });
+    }
+).catch((err) => {console.log(`Error: ${error}`)});
