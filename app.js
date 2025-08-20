e5334c66112ab60415d460e789a6435e015b41d7
Vinay Summan <vinay.summan@rdpolytech.ca>
2025-08-20 17:55:21 -0600

    update

--- app.js
+++ app.js
@@ -5,6 +5,19 @@ const mongoose = require('mongoose');
 
 const PORT = process.env.PORT;
 const uri = process.env.MONGO_URI;
+const ejs = require('ejs');
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+//route handlers for the home
+app.get('/home', (req, res) => {
+    res.render('home');
+});
 
 mongoose.connect(uri).then(
     async () => {
@@ -15,4 +28,4 @@ mongoose.connect(uri).then(
         console.log(`Connected to port ${PORT}`);
         });
     }
-).catch((err) => {console.log(`Error: ${error}`)});
+).catch((err) => {console.log(`Error: ${err}`)});
